228ee6d6e719b798f9eb18e3ee155f52a6ae8aa2
user <>
2024-02-16 17:38:04 +0300

    add example page

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,176 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Page(),
+    return MaterialApp(
+      home: HomePage(),
     );
   }
 }
 
-class Page extends StatefulWidget {
-  const Page({super.key});
+final listOfMovements = [
+  [
+    CircleMovement(1, 100),
+    CircleMovement(1, 27 / 20 * 100),
+    CircleMovement(4, 19 / 20 * 100),
+    CircleMovement(-5, 11 / 10 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-9, 1 / 20 * 100),
+    CircleMovement(10, 21 / 20 * 100),
+    // CircleMovement(-5, 11 / 10 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-1, 27 / 10 * 100),
+    CircleMovement(-3, 17 / 20 * 100),
+    CircleMovement(4, 3 / 4 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(9, 17 / 20 * 100),
+    CircleMovement(-4, 29 / 20 * 100),
+    CircleMovement(2, 61 / 20 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-4, 11 / 10 * 100),
+    CircleMovement(-2, 19 / 10 * 100),
+    CircleMovement(4, 4 / 5 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-7, 11 / 15 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-7, 1 / 2 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-3, 3 / 10 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-5, 1 / 10 * 100),
+    CircleMovement(13, 1 / 20 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-3, 7 / 20 * 100),
+    CircleMovement(6, 1 / 4 * 100),
+    CircleMovement(-12, 1 / 10 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-5, 1 * 100),
+    CircleMovement(7, 17 / 20 * 100),
+    // CircleMovement(4, 4 / 5 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-8, 1 * 100),
+    CircleMovement(10, 11 / 10 * 100),
+    CircleMovement(1, 3 / 5 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-11, 1 * 100),
+    CircleMovement(9, 23 / 20 * 100),
+    // CircleMovement(1, 3/5 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-5, 1 * 100),
+    CircleMovement(4, 1 * 100),
+    // CircleMovement(1, 3/5 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-15, 1 * 100),
+    CircleMovement(-2, 1 * 100),
+    CircleMovement(14, 11 / 10 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-1, 1 * 100),
+    CircleMovement(-12, 1 * 100),
+    CircleMovement(11, 1 * 100),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(-1, 11 / 10 * 100),
+    CircleMovement(-2, 37 / 20 * 100),
+    CircleMovement(-5, 3 / 2 * 100),
+  ],
+  [
+    CircleMovement(1, 50),
+    CircleMovement(10, 19 / 20 * 50),
+    CircleMovement(-8, 13 / 10 * 50),
+    CircleMovement(4, 5 * 50),
+  ],
+  [
+    CircleMovement(1, 100),
+    CircleMovement(3, 1 * 100),
+    CircleMovement(2, 1 * 100),
+    CircleMovement(-5, 19 / 20 * 100),
+  ],
+  [
+    CircleMovement(1, 50),
+    CircleMovement(1, 27 / 20 * 50),
+    CircleMovement(4, 19 / 20 * 50),
+    CircleMovement(-5, 11 / 10 * 50),
+  ],
+  [
+    CircleMovement(1, 50),
+    CircleMovement(10, 1 * 50),
+    CircleMovement(-5, 1 * 50),
+    CircleMovement(-5, 7 / 4 * 50),
+  ],
+];
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
 
   @override
-  State<Page> createState() => _PageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          children: [
+            for (var (n, movemnts) in listOfMovements.indexed)
+              ListTile(
+                title: Center(child: Text('Example #${n + 1}')),
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => PageWithCurve(
+                        title: Text('Example #${n + 1}'),
+                        movements: movemnts,
+                      ),
+                    ),
+                  );
+                },
+              )
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _PageState extends State<Page> with TickerProviderStateMixin {
+class PageWithCurve extends StatefulWidget {
+  final Widget title;
+  final List<CircleMovement> movements;
+  const PageWithCurve(
+      {super.key, required this.movements, required this.title});
+
+  @override
+  State<PageWithCurve> createState() => _PageWithCurveState();
+}
+
+class _PageWithCurveState extends State<PageWithCurve>
+    with TickerProviderStateMixin {
   late final AnimationController controller;
 
   @override
@@ -38,23 +194,18 @@ class _PageState extends State<Page> with TickerProviderStateMixin {
 
   @override
   void dispose() {
-    super.dispose();
     controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: widget.title),
       body: Center(
         child: CurveAnimation(
           animation: controller,
-          movements: [
-            CircleMovement(1, 100),
-            CircleMovement(1, 27 / 20 * 100),
-            CircleMovement(4, 19 / 20 * 100),
-            CircleMovement(-5, 11 / 10 * 100),
-            // Movement(1, 3/5*100),
-          ],
+          movements: widget.movements,
         ),
       ),
     );
@@ -81,6 +232,7 @@ class CurveAnimation extends AnimatedWidget {
     this.movements = const [],
     required Animation<double> animation,
   }) : super(listenable: animation) {
+    // FIXME генерировать не сразу все точки
     _generatePoints();
   }
 
@@ -104,6 +256,7 @@ class CurveAnimation extends AnimatedWidget {
         .map((e) => e.denominator.abs())
         .reduce((lcm, n) => lcm * n ~/ lcm.gcd(n));
 
+    //FIXME вынести n в конструктор
     int n = 1000;
     double start = 0;
     double end = 2 * pi * reducedDenominatorProduct;
